0c47e7d1683fb08c83a5ce7414a6415c028594ec
GayanShyamal <gayanshyamal706@gmail.com>
2026-04-26 21:40:12 +0530

    Create New class

---
+++ src/Student.java
@@ -0,0 +1,29 @@
+public class Student {
+    String name;
+    String email;
+
+    //No argument constructor
+    public Student() {
+    }
+
+    public Student(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
